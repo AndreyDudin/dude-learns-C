f11a8b211dc9ced19b08fbc60f86620e8e30aaff
Andrey dudin <program95@mail.ru>
2014-04-17 01:56:55 +0400

    full version of bad code

--- lab3.c
+++ lab3.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 
-
+const int ALL_VERSIONS = 0;
 typedef struct Item
 {
     int key;
@@ -12,72 +13,256 @@ typedef struct Item
 } Item;
 typedef Item *list_t;
 
-list add_element(list_t head, char *inf, int k);
-void search(list_t head, int k, int version);
 
-
-
+list_t delete_one(list_t head, int k, int version);
+list_t delete_all(list_t head, int k);
+list_t search(list_t head);
+list_t delete_t(list_t head);
+list_t print_t(list_t head);
+list_t add_element(list_t head);
 
 int main()
 {
-    Item list =  NULL;
+
+
+    int k, version;
+    int i = 0;
+    char *str[80];
+    list_t temp;
+    list_t head;
+    setlocale (LC_ALL, "Rus");
+    for (;;)
+    {
+        list_t list =  NULL;
+        while (i < 3)
+        {
+            list = add_element(list);
+            i++;
+
+        }
+        list = print_t(list);
+        list = delete_t(list);
+        list = print_t(list);
+
+    }
+    return 0;
+
 }
 
 
-
-
-list add_element(list_t head, char *inf, int k)
+list_t delete_t(list_t head)
 {
+    int k;
+    int version;
+
     list_t new_head;
+    printf("vvedite klych\n");
+    scanf("%d", &k);
+    fflush(stdin);
+    printf("Vvedite version, for delete enter 0\n");
+    scanf("%d", &version);
+    fflush(stdin);
+    if (version == ALL_VERSIONS)
+    {
+        new_head = delete_all(head, k);
+    }
+    if (version != ALL_VERSIONS)
+    {
+        new_head = delete_one(head, k, version);
+    }
+    return new_head;
+}
+
+list_t print_t(list_t head)
+{
     list_t current;
-    list_t new_head = malloc(sizeof(Item));
+    current = head;
+    while (current != NULL)
+    {
+        printf("%d %d %s ", current->key, current->release, &(current->info));
+        current = current->next;
+    }
+    return head;
+}
+
+
+list_t add_element(list_t head)
+{
+
+    list_t current;
+    list_t new_head = (Item *)malloc(sizeof(Item));
+    int k;
+
+    char *str[80];
+    printf("Vvedite klych\n");
+    scanf("%d", &k);
+    fflush(stdin);;
+    printf("Vvedite info\n");
+    scanf("%80[^\n]", str);
+    fflush(stdin);
+
+
     new_head->next = head;
-    new_head->info = inf;
+    new_head->info = *str;
     new_head->key = k;
-    current = new_head;
-    while ( current->next != NULL)
+    current = head;
+    new_head->release = 1;
+
+    while ( current != NULL)
     {
         if (current->key == k)
         {
             new_head->release = current->release + 1;
             break;
         }
+        current = current->next;
+
 
 
     }
-    if (new_head->release < 1)
+
+    return new_head;
+
+
+}
+
+list_t delete_one(list_t head, int k, int version)
+{
+    list_t current;
+    list_t temp;
+    list_t prev;
+
+    if (head == NULL)
     {
-        new_head->release = 1;
+        printf("Таблица пуста\n");
+        return head;
     }
+    else
+    {
+        if (head->key == k && head->release == version)
+        {
+            temp = head;
+            head = head->next;
+            free(temp->info);
+            free(temp);
+            return head;
+        }
+        current = head->next;
+        prev = head;
+        while (current != NULL)
+        {
+            if (current->key == k && current->release == version)
+            {
+                temp = current;
+                prev->next = current->next;
+                free(temp->info);
+                free(temp);
+                return head;
+
+
+            }
+            current = current->next;
+            prev = prev->next;
+        }
+
+    }
+
+    return head;
+
+}
+list_t delete_all(list_t head, int k)
+{
+    list_t current;
+    list_t temp;
+    list_t prev;
+    if (head == NULL)
+    {
+        printf("Таблица пуста\n");
+        return head;
+    }
+    else
+    {
+        while (head->key == k && head != NULL)
+        {
+            temp = head;
+            head = head->next;
+            free(temp->info);
+            free(temp);
+        }
+        if (head == NULL)
+        {
+            return head;
+        }
+        else
+        {
+            prev = head;
+            current = head->next;
+            while (current != NULL)
+            {
+                if (current->key == k)
+                {
+                    temp = current;
+                    prev->next = current->next;
+                    current = current->next;
+                    free(temp->info);
+                    free(temp);
+
+                }
+                else
+                {
+                    current = current->next;
+                    prev = prev->next;
+                }
+
+            }
+        }
+
+
+    }
+    return head;
+
 
 }
 
 
 
 
-void search(list_t head, int k, int version)
+list_t search(list_t head)
 {
     list_t current;
+    int k, version;
     current = head;
-    if (version < 999)
+
+    printf("Vvedite kluch dlya poiska\n");
+    scanf("%d", &k);
+    fflush(stdin);
+    printf("Vvedite version for search all 0\n");
+    scanf("%d", &version);
+    fflush(stdin);
+
+
+    if (version != ALL_VERSIONS)
     {
-        while (current != 0)
+        while (current != NULL)
         {
             if (current->key == k && current->release == version)
-                printf("%d %d %s", current->key, current->release, current->info);
+                printf("%d %d %s", current->key, current->release, &(current->info));
             current = current->next;
+
         }
+
     }
 
-    if (version == 999)
+    if (version == ALL_VERSIONS)
     {
-        while (current != 0)
+        while (current != NULL)
         {
             if (current->key == k)
-                printf("%d %d %s", current->key, current->release, current->info);
+                printf("%d %d %s", current->key, current->release, &(current->info));
             current = current->next;
         }
     }
+    return head;
 
 
 }
