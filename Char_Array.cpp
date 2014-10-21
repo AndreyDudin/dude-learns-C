14009b106e4e4021bc81cedd16fab9a43e3c033b
Andrey dudin <program95@mail.ru>
2014-10-21 22:25:42 +0400

    Exceprions

--- Char_Array.cpp
+++ Char_Array.cpp
@@ -12,6 +12,28 @@ Char_Array::Char_Array()
 	ar = nullptr;
 }
 
+
+Char_Array::Char_Array(Char_Array && ob) :amount_of_words(ob.amount_of_words), ar(ob.ar)
+
+{
+	ob.ar = nullptr;
+}
+
+
+Char_Array& Char_Array::operator=(Char_Array && ob)
+{
+	int tamount_of_words = amount_of_words;
+	amount_of_words = ob.amount_of_words;
+	ob.amount_of_words = tamount_of_words;
+	char** tar = ar;
+	ar = ob.ar;
+	ob.ar = tar;
+	return *this;
+}
+
+
+
+
 Char_Array::Char_Array(const Char_Array &ob) :amount_of_words(ob.amount_of_words), ar(nullptr) ///???
 {
 	if (amount_of_words > 0)
@@ -26,7 +48,7 @@ Char_Array::Char_Array(const Char_Array &ob) :amount_of_words(ob.amount_of_words
 				strcpy(ar[i], ob.ar[i]);
 			}
 
-			catch (std::bad_alloc t)
+			catch (std::bad_alloc &t)
 			{
 				for (int i = 0; i < amount_of_words; i++)
 				{
@@ -35,7 +57,7 @@ Char_Array::Char_Array(const Char_Array &ob) :amount_of_words(ob.amount_of_words
 				delete[]ar;
 				throw t;
 			}
-			
+
 
 		}
 	}
@@ -45,25 +67,34 @@ Char_Array& Char_Array::operator =(const Char_Array &ob)
 {
 	if (this != &ob)
 	{
-		
+
 		for (int i = 0; i < amount_of_words; i++)
 		{
 			delete[]ar[i];
 		}
 		delete[]ar;
 		ar = nullptr;
-		if ((amount_of_words=ob.amount_of_words) != 0)
-		{
-			ar = new char*[ob.amount_of_words];
-			for (int i = 0; i < amount_of_words; i++)
+		try{
+			if ((amount_of_words = ob.amount_of_words) != 0)
 			{
-				ar[i] = new char[strlen(ob.ar[i]) + 1];
-				strcpy(ar[i], ob.ar[i]);
+				ar = new char*[ob.amount_of_words];
+				for (int i = 0; i < amount_of_words; i++)
+				{
+					ar[i] = new char[strlen(ob.ar[i]) + 1];
+					strcpy(ar[i], ob.ar[i]);
 
+				}
 			}
 		}
-
-
+		catch (std::bad_alloc &t)
+		{
+			for (int i = 0; i < amount_of_words; i++)
+			{
+				delete[]ar[i];
+			}
+			delete[]ar;
+			throw t;
+		}
 
 	}
 	return *this;
@@ -71,19 +102,17 @@ Char_Array& Char_Array::operator =(const Char_Array &ob)
 
 Char_Array::Char_Array(const char *s)
 	:amount_of_words(1)
-	
+
 {
-	
-	
 	ar = new char*[amount_of_words];
 
 	try
 	{
 		ar[0] = new char[strlen(s) + 1];//catch(std::bad_alloc)
-		strcpy(ar[0],s);
+		strcpy(ar[0], s);
 	}
 
-	catch (std::bad_alloc t)
+	catch (std::bad_alloc &t)
 	{
 		for (int i = 0; i < amount_of_words; i++)
 		{
@@ -98,7 +127,6 @@ Char_Array::Char_Array(const char *s)
 Char_Array::Char_Array(const int n, const char** mas)
 
 {
-	
 	amount_of_words = n;
 	ar = new char*[amount_of_words];
 	for (int i = 0; i < amount_of_words; i++)
@@ -109,7 +137,7 @@ Char_Array::Char_Array(const int n, const char** mas)
 			strcpy(ar[i], mas[i]);
 		}
 
-		catch (std::bad_alloc t)
+		catch (std::bad_alloc &t)
 		{
 			for (int i = 0; i < amount_of_words; i++)
 			{
@@ -118,7 +146,6 @@ Char_Array::Char_Array(const int n, const char** mas)
 			delete[]ar;
 			throw t;
 		}
-
 
 	}
 }
@@ -137,22 +164,41 @@ std::istream& operator>>(std::istream& is, Char_Array& ch)
 {
 	char c;
 	std::string s;
+	for (int i = 0; i < ch.amount_of_words; i++)
+		delete[] ch.ar[i];
+	delete[] ch.ar;
+	ch.ar = nullptr;
 	ch.amount_of_words = 0;
 	do {
 		is >> s;
-		
-
-		ch +=s.c_str();
-		
-
-
-
+		try
+		{
+			ch += s.c_str();
+		}
+		catch (std::bad_alloc &t)
+		{
+			is.setstate(std::ios::failbit);
+			std::cerr << "failbit was set" << std::endl;
+			return is;
+		}
 
 		c = is.get();
 		if (c == '\n') break;
 		if (c == ' ') continue;
 		is.unget();
 	} while (true);
+	for (int i = 0; i < ch.amount_of_words - 1; i++)
+	{
+		for (int j = i + 1; j < ch.amount_of_words; j++)
+		{
+			if (strcmp(ch.ar[i], ch.ar[j]) == 0)
+			{
+				is.setstate(std::ios::failbit);
+				std::cerr << "failbit was set" << std::endl;
+				return is;
+			}
+		}
+	}
 	return is;
 }
 
@@ -195,36 +241,35 @@ void Char_Array::sort()
 
 Char_Array& Char_Array::operator +=(const Char_Array &s)
 {
-	
-	
+
+	int n = amount_of_words;
 	char **new_ar = new char*[amount_of_words + s.amount_of_words];
 	try
 	{
 		for (int i = 0; i < amount_of_words; i++)
 		{
-			new_ar[i] = new char[strlen(ar[i])];
+			new_ar[i] = new char[strlen(ar[i]) + 1];
 			strcpy(new_ar[i], ar[i]);
 		}
-	}
-	catch (std::bad_alloc t)
-	{
 
-		throw t;
-	}
-	for (int i = 0; i < s.amount_of_words; i++)
-	{
-		try
+
+		for (int i = 0; i < s.amount_of_words; i++)
 		{
-
-			new_ar[amount_of_words] = new char[strlen(s.ar[i])];
+			new_ar[amount_of_words] = new char[strlen(s.ar[i]) + 1];
 			strcpy(new_ar[amount_of_words++], s.ar[i]);
 		}
-		catch (std::bad_alloc t)
-		{
-			
-			throw t;
-		}
 	}
+	catch (std::bad_alloc &t)
+	{
+		throw t;
+	}
+	if (ar)
+	{
+		for (int i = 0; i < n; i++)
+			delete[] ar[i];
+		delete[] ar;
+	}
+	
 	ar = new_ar;
 	return *this;
 }
@@ -239,18 +284,24 @@ Char_Array& Char_Array::operator +=(const char *s)
 		for (int i = 0; i < amount_of_words; i++)
 		{
 
-			new_ar[i] = new char[strlen(ar[i])];
+			new_ar[i] = new char[strlen(ar[i])+1];
 			strcpy(new_ar[i], ar[i]);
-
 		}
 
 		new_ar[amount_of_words] = new char[strlen(s) + 1];
 		strcpy(new_ar[amount_of_words], s);
 	}
-	catch (std::bad_alloc t)
+	catch (std::bad_alloc &t)
 	{
 		throw t;
 	}
+	if (ar)
+	{
+		for (int i = 0; i < amount_of_words; i++)
+			delete[] ar[i];
+		delete[] ar;
+	}
+
 	amount_of_words++;
 	ar = new_ar;
 	return *this;
@@ -280,20 +331,19 @@ Char_Array Char_Array::operator () (const char c)
 
 		}
 	}
-
 	return ch;
 }
 
 Char_Array::~Char_Array()
 {
-	
-	for (int i = 0; i < amount_of_words; i++)
+
+	for (int i = amount_of_words - 1; i>=0; i--)
 	{
-		delete ar[i];
+		delete[] ar[i];
 	}
 	delete[]ar;
 	ar = nullptr;
 
-	
+
 
 }
--- Char_Array.h
+++ Char_Array.h
@@ -1,8 +1,6 @@
 
 #pragma once
 #define _CRT_SECURE_NO_WARNINGS
-#define  MAXLENGTH 80
-#define  MAXAMOUNT 80
 #include <iostream>
 using namespace std;
 class Char_Array
@@ -15,6 +13,8 @@ public:
 	Char_Array(const char *s);
 	Char_Array(const int n, const char** mas);
 	Char_Array(const Char_Array &ob);
+	Char_Array(Char_Array&& ob);
+	//move constr
 	virtual ~Char_Array();
 
 	friend std::ostream& operator<<(std::ostream& os, const Char_Array& ch);
@@ -23,7 +23,8 @@ public:
 	Char_Array& operator +=(const char *s);
 	Char_Array& operator +=(const Char_Array &s);
 	Char_Array Char_Array::operator () (const char c);
-	void operator () (char c, Char_Array& res) const;
+	Char_Array& operator =(const Char_Array &ob);
+	Char_Array& operator =( Char_Array &&ob);
 	const char* operator [](int) const;
 
 	void sort();
